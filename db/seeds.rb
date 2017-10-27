ff6230672b642dee665271cc5b85acbfd51c544e
Faye Hayes <fhayes30@gmail.com>
2017-10-27 13:58:01 -0700

    Add enum status for approved,rejected, and submitted

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@
 
 puts "1 User created"
 
+@admin = AdminUser.create!(email: "admin@test.com", first_name: "Admin", last_name: "Name", password: "asdfasdf", password_confirmation: "asdfasdf")
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rational content", user_id: @user.id)
 end
