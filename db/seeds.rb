52935bc3efdf425c3cba1a8b2c7cc9f816395fac
Faye Hayes <fhayes30@gmail.com>
2017-10-31 15:53:39 -0700

    Add overtime_request hours to post model - test is failing for user authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 
-@user = User.create!(email: "test@test.com", first_name: "jon", last_name: "smith", password: "asdfasdf", password_confirmation: "asdfasdf")
+@user = User.create!(email: "tester@test.com", first_name: "jon", last_name: "smith", password: "asdfasdf", password_confirmation: "asdfasdf")
 
 puts "1 User created"
 
-@admin = AdminUser.create!(email: "admin@test.com", first_name: "Admin", last_name: "Name", password: "asdfasdf", password_confirmation: "asdfasdf")
+@admin = AdminUser.create!(email: "2admin@test.com", first_name: "Admin", last_name: "Name", password: "asdfasdf", password_confirmation: "asdfasdf")
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rational content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rational content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
