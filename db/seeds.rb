fbed84d63acb02b13c5b1d7e44ed43a29a5e896d
Faye Hayes <fhayes30@gmail.com>
2017-11-04 18:14:55 -0700

    Add phone requirement to seeds file, spec files, and user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
 
-@user = User.create!(email: "tester@test.com", first_name: "jon", last_name: "smith", password: "asdfasdf", password_confirmation: "asdfasdf")
+@user = User.create!(email: "tester@test.com",
+                    first_name: "jon",
+                    last_name: "smith",
+                    password: "asdfasdf",
+                    password_confirmation: "asdfasdf",
+                    phone: "5555555555")
 
 puts "1 User created"
 
-@admin = AdminUser.create!(email: "2admin@test.com", first_name: "Admin", last_name: "Name", password: "asdfasdf", password_confirmation: "asdfasdf")
+@admin = AdminUser.create!(email: "2admin@test.com",
+                            first_name: "Admin",
+                            last_name: "Name",
+                            password: "asdfasdf",
+                            password_confirmation: "asdfasdf",
+                            phone: "5555555555")
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rational content", user_id: @user.id, overtime_request: 2.5)
